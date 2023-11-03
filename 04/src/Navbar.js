2525b540af88887bc9be0d9f5c53087858bda71f
Milad Joodi <winbeta.com@gmail.com>
2023-11-04 00:27:11 +0330

    0

---
+++ 04/src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div className='container'>
+                <div className='nav'></div>
+                <div className=''></div>
+                <ul></ul>
+        </div>
+    </nav>
+  )
+}
---
+++ 04/src/data.js
@@ -0,0 +1,48 @@
+import React from 'react;'
+import {FaBehance, FaFacebook, FaLinkedin, FaTwitter} from 'react-icons/fa';
+
+export const links = [
+    {
+        id: 1,
+        url: '/',
+        text: 'خانه'
+    },
+    {
+        id: 2,
+        url: '/',
+        text: 'درباره ما'
+    },
+    {
+        id: 3,
+        url: '/',
+        text: 'تماس با ما'
+    },
+    {
+        id: 4,
+        url: '/',
+        text: 'پروژه های ما'
+    },
+]
+
+export const socials = [
+    {
+        id: 1,
+        url: 'http://twitter.com',
+        icon: <FaBehance />
+    },
+    {
+        id: 2,
+        url: 'http://twitter.com',
+        icon: <FaFacebook />
+    },
+    {
+        id: 3,
+        url: 'http://twitter.com',
+        icon: <FaLinkedin />
+    },
+    {
+        id: 4,
+        url: 'http://twitter.com',
+        icon: <FaTwitter />
+    },
+]
