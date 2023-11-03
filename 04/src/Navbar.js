36a452ddde859eea09c96f93efaf5789006332c8
Milad Joodi <winbeta.com@gmail.com>
2023-11-04 01:28:40 +0330

    up

--- 04/src/Navbar.js
+++ 04/src/Navbar.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from "react";
+import logo from "./logo.png";
+import { FaBars, FaTwitter, FaTimes } from "react-icons/fa";
+import { links, socials } from "./data";
 
 export default function Navbar() {
   return (
     <nav>
-        <div className='container'>
-                <div className='nav'></div>
-                <div className=''></div>
-                <ul></ul>
-        </div>
+      <div className="container">
+                    <div className="nav-logo">
+                         <button className="nav-toggle">
+                              <FaBars />
+                         </button>
+                         <img src={logo} alt="" />
+                    </div>
+                    <div className="nav-links">
+                         <ul className="list">
+                         {
+                              links.map((link) => {
+                                   const {id,url,text} = link;
+                                   return (
+                                        <li key={id}>
+                                             <a href={url}>{text}</a>
+                                        </li>
+                                   )
+                              })
+                         }
+                         </ul>
+                    </div>
+        <ul className="nav-social">
+            {
+                socials.map((social)=> {
+                    const {id, url, icon} = social;
+                    return (
+                        <li key={id}>
+                            <a href={url}>{icon}</a>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
