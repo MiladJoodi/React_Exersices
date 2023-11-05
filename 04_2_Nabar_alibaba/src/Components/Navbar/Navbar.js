000539096de4a7f8733cf356bdc54eceedc0c613
Milad Joodi <winbeta.com@gmail.com>
2023-11-05 06:49:22 +0330

    working at alibaba projects

--- 04_2_Nabar_alibaba/src/Components/Navbar/Navbar.js
+++ 04_2_Nabar_alibaba/src/Components/Navbar/Navbar.js
@@ -1,45 +1,47 @@
-import React, { useState } from 'react'
-import {rightLinks, leftLinks} from './data'
-import logo from '../../logo.svg' 
+import React, { useState } from "react";
+import { rightLinks, leftLinks } from "./data";
+import logo from "../../logo.svg";
+import "./Navbar.css";
 
 export default function Navbar() {
-
-    const [rightLink, setRightLinks] = useState(rightLinks)
-    const [leftLink, setLeftLinks] = useState(leftLinks)
-
+  const [rightLink, setRightLinks] = useState(rightLinks);
+  const [leftLink, setLeftLinks] = useState(leftLinks);
 
   return (
-    <div className='navbar'>
+    <div className="navbar">
+      <div className="container">
         {/* right */}
-        <div className='navbar-right'>
-            <div className='navbar-right-logo'>
-                <img src={logo} />
-            </div>
-            <ul className='navbar-right-links'>
-                {rightLink.map((link)=>{
-                    const {id, url, title} = link
-                    return (
-                        <li key={id}>
-                            <a href={url}>{title}</a>
-                        </li>
-                    )
-                })
-                }
-            </ul>
-        </div>
-
-        {/* left */}
-        <ul className='navbar-left'>
-            {leftLink.map((link)=>{
-                const {id, url, title, icon} = link;
-                return (
-                    <li key={id}>
-                        <a href={url}>{icon} {title}</a>
-                    </li>
-                )
-                
+        <div className="navbar-right">
+          <div className="navbar-right-logo">
+            <img src={logo} />
+          </div>
+          <ul className="navbar-right-links">
+            {rightLink.map((link) => {
+              const { id, url, title, icon } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>
+                     {title} {icon}
+                  </a>
+                </li>
+              );
             })}
+          </ul>
+        </div>
+        {/* left */}
+        <ul className="navbar-left">
+          {leftLink.map((link) => {
+            const { id, url, title, icon } = link;
+            return (
+              <li key={id}>
+                <a href={url}>
+                  {icon} {title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
+      </div>
     </div>
-  )
+  );
 }
--- 04_2_Nabar_alibaba/src/Components/Navbar/data.js
+++ 04_2_Nabar_alibaba/src/Components/Navbar/data.js
@@ -1,50 +1,51 @@
 import React from "react";
 import { AiOutlineQuestionCircle, AiOutlineUser } from "react-icons/ai";
 import { LuBaggageClaim } from "react-icons/lu";
-
-
-
+import { IoIosArrowDown } from "react-icons/io";
 
 export const rightLinks = [
-    {
-        id: 1,
-        url: '/tickets',
-        title: 'بلیط'
-    },
-    {
-        id: 2,
-        url: '/residence',
-        title: 'اقامت'
-    },
-    {
-        id: 3,
-        url: '/tour',
-        title: 'تور'
-    },
-    {
-        id: 4,
-        url: '/more',
-        title: 'بیشتر'
-    },
-]
+  {
+    id: 1,
+    url: "/tickets",
+    title: "بلیط",
+    icon: <IoIosArrowDown />,
+  },
+  {
+    id: 2,
+    url: "/residence",
+    title: "اقامت",
+    icon: <IoIosArrowDown />,
+  },
+  {
+    id: 3,
+    url: "/tour",
+    title: "تور",
+  },
+  {
+    id: 4,
+    url: "/more",
+    title: "بیشتر",
+    icon: <IoIosArrowDown />,
+  },
+];
 
 export const leftLinks = [
-    {
-        id: 1,
-        url: '/support',
-        title: 'مرکز پشتیبانی آنلاین',
-        icon: <AiOutlineQuestionCircle />
-    },
-    {
-        id: 2,
-        url: '/trips',
-        title: 'سفرهای من',
-        icon: <LuBaggageClaim />
-    },
-    {
-        id: 3,
-        url: '/login',
-        title: 'ورود یا ثبت نام',
-        icon: <AiOutlineUser />
-    },
-]
+  {
+    id: 1,
+    url: "/support",
+    title: "مرکز پشتیبانی آنلاین",
+    icon: <AiOutlineQuestionCircle />,
+  },
+  {
+    id: 2,
+    url: "/trips",
+    title: "سفرهای من",
+    icon: <LuBaggageClaim />,
+  },
+  {
+    id: 3,
+    url: "/login",
+    title: "ورود یا ثبت نام",
+    icon: <AiOutlineUser />,
+  },
+];
