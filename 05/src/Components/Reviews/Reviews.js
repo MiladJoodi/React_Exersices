e7060539d24d63bb3ab0ea5f92c972158c2fabbe
Milad Joodi <winbeta.com@gmail.com>
2023-11-08 12:14:12 +0330

    a

--- 05/src/Components/Reviews/Reviews.js
+++ 05/src/Components/Reviews/Reviews.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reviews.css";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+// import Typewriter from 'typewriter-effect/dist/core';
+import Typewriter from 'typewriter-effect';
+import data from '../../data'
 
 export default function Reviews() {
+
+  const [index, setIndex] = useState(0)
+  const {name, job, image} = data[index];
+
+  console.log(data);
+
   return (
     <div className="reviews">
       <div className="review-img">
-        <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <img src={image} />
       </div>
-      <h4 className="author">جودی</h4>
-      <p className="job">گرافیست</p>
+      <h4 className="author">{name}</h4>
+      <Typewriter
+            className="job"
+            onInit={(typeWriter)=>{
+            typeWriter
+              .typeString({job})
+              .start()
+              .pauseFor(2000)
+              .deleteAll()
+              .start()
+            }}
+            options={{
+              loop: true,
+            }}
+            />
+
+      {/* <p className="job">گرافیست</p> */}
       <p className="info">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز است.</p>
       <div className="review-button">
         <button><FaChevronRight /></button>
