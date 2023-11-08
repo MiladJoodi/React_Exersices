15e73a26a4955d89515198a25179b26f94cb417e
Milad Joodi <winbeta.com@gmail.com>
2023-11-08 15:18:17 +0330

    ♥

--- 05/src/Components/Reviews/Reviews.js
+++ 05/src/Components/Reviews/Reviews.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Reviews.css";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 // import Typewriter from 'typewriter-effect/dist/core';
@@ -8,9 +8,42 @@ import data from '../../data'
 export default function Reviews() {
 
   const [index, setIndex] = useState(0)
-  const {name, job, image} = data[index];
+  const {name, job, image, text } = data[index];
 
-  console.log(data);
+  console.log(index);
+
+  const checkNumber = (number)=>{
+    if(number > data.length-1){
+      return 0
+    }
+    if(number < 0) { 
+      return data.length-1
+    }
+    return number
+  }
+
+  const nextSlide = ()=>{
+    setIndex(index=> {
+      let newIndex = index + 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  const prevSlide = ()=>{
+    setIndex(index=> {
+      let newIndex = index - 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  const randomPerson = ()=> {
+    let randomNumber = Math.floor(Math.random() * data.length)
+    if(randomNumber === index){
+      randomNumber = index+1
+    }
+    console.log(randomNumber);
+    setIndex(checkNumber(randomNumber))
+  }
 
   return (
     <div className="reviews">
@@ -18,11 +51,12 @@ export default function Reviews() {
         <img src={image} />
       </div>
       <h4 className="author">{name}</h4>
-      <Typewriter
+      <h4 className="job">{job}</h4>
+      {/* <Typewriter
             className="job"
             onInit={(typeWriter)=>{
             typeWriter
-              .typeString({job})
+              .typeString(job)
               .start()
               .pauseFor(2000)
               .deleteAll()
@@ -31,15 +65,15 @@ export default function Reviews() {
             options={{
               loop: true,
             }}
-            />
+            /> */}
 
       {/* <p className="job">گرافیست</p> */}
-      <p className="info">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز است.</p>
+      <p className="info">{text}</p>
       <div className="review-button">
-        <button><FaChevronRight /></button>
-        <button><FaChevronLeft /></button>
+        <button onClick={nextSlide}><FaChevronRight /></button>
+        <button onClick={prevSlide}><FaChevronLeft /></button>
       </div>
-      <button className="random-btn">تصادفی</button>
+      <button onClick={randomPerson} className="random-btn">تصادفی</button>
     </div>
   );
 }
--- 05/src/data.js
+++ 05/src/data.js
@@ -3,15 +3,15 @@ const reviews = [
         id: 1,
         name: 'فرزاد معصومی',
         job: 'طراح وب',
-        image: 'https://images.pexels.com/photos/9154716/pexels-photo-9154716.jpeg?cs=srgb&dl=pexels-cottonbro-9154716.jpg&fm=jpg',
-        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+        image: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        text: 'سرعت لود فوق العاده، امنیت بالا، احترام به حقوق مشتری و پشتیبانی ۲۴/۷ ساعته حرفه ای را با هاست‌ دی ‌ال تجربه کنید! آسان دانلود ضمانت خرید شما با تشکر از زحمات مدیریت محترم جناب آقای مهندس عزیز  آسان دانلود',
     
     },
     {
         id: 2,
         name: 'شایان شهینی',
         job: 'گرافیست',
-        image: 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        image: 'https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
     
     },
@@ -19,7 +19,7 @@ const reviews = [
         id: 3,
         name: 'شهین شایانی',
         job: 'برنامه نویس',
-        image: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        image: 'https://images.pexels.com/photos/3914102/pexels-photo-3914102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ت',
     
     }
