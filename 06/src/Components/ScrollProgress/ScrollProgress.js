0a9b2cadbd8c53c5dea222e2c01029ef79c3f148
Milad Joodi <winbeta.com@gmail.com>
2023-11-09 04:00:18 +0330

    hi

---
+++ 06/src/Components/ScrollProgress/ScrollProgress.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ScrollProgress.css'
+
+export default function ScrollProgress() {
+  return (
+    <div className='wrapper'>
+        <div className='progressbar'>
+            <div className='progressbarMain'></div>
+        </div>
+    </div>
+  )
+}
