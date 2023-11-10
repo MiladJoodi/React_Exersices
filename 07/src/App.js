d0e68353ae52050f0664ea6c809b3256fea0c077
Milad Joodi <winbeta.com@gmail.com>
2023-11-10 03:33:48 +0330

    finish search image project.

--- 07/src/App.js
+++ 07/src/App.js
@@ -2,13 +2,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  // U7rtiKUp444M9xk6ImZnyzXbYzVKiULEvxOH2jkR2Kg
+  const [value, setvalue] = useState(null)
+  const [gallery, setGallery] = useState(null)
+
+  const searchBtn = ()=>{
+    fetch(`https://api.unsplash.com/search/photos/?client_id=U7rtiKUp444M9xk6ImZnyzXbYzVKiULEvxOH2jkR2Kg&query=${value}>`)
+    .then(res=> res.json())
+    .then(data=> {
+      console.log(data.results);
+      setGallery(data.results)
+    })
+  }
+
   return (
     <div className="app">
       {/*start searchbar */}
       <div className="search-bar">
         <div class="input-group rounded">
           <span class="input-group-text border-0" id="search-addon">
-            <button type="button" class="btn btn-danger">
+            <button type="button" class="btn btn-danger" onClick={searchBtn}>
               جستجو کن
             </button>
           </span>
@@ -18,13 +32,15 @@ function App() {
             placeholder="جستجو"
             aria-label="Search"
             aria-describedby="search-addon"
+            value={value}
+            onChange={(e)=> setvalue(e.target.value)}
           />
         </div>
       </div>
       {/*end searchbar */}
 
       <div className="gallery">
-        <img src="https://images.pexels.com/photos/18684565/pexels-photo-18684565/free-photo-of-old-tenements-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        {gallery && gallery.map((item)=><img src={item.urls.regular} alt="" />)}
       </div>
     </div>
   );
