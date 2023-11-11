475f676f02183e68bfc9a06d7bb00bec27ad43bf
Milad Joodi <winbeta.com@gmail.com>
2023-11-11 18:25:18 +0330

    start new Project

---
+++ 10/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { Icon } from "react-icons-kit";
+import { eye } from "react-icons-kit/feather/eye";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+
+function App() {
+
+  
+
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
