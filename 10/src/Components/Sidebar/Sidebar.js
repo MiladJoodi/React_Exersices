bb892d19226e7413ccbc90af1a6b8327dab8a1ba
Milad Joodi <winbeta.com@gmail.com>
2023-11-12 00:08:49 +0330

    ♥

--- 10/src/Components/Sidebar/Sidebar.js
+++ 10/src/Components/Sidebar/Sidebar.js
@@ -1,28 +1,37 @@
-import React from 'react'
-import './Sidebar.css'
-import {FaTimes} from 'react-icons/fa'
-import Background from './Background'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { FaTimes } from "react-icons/fa";
+import Background from "./Background";
+import {links} from "../../data";
 
 export default function Sidebar() {
+  const [sidebarLinks, setSidebarLinks] = useState(links);
 
-    
+//   console.log(sidebarLinks);
+
   return (
-    <div className='sidebar'>
-        <div className="container">
+    <div className="sidebar">
+      <div className="container">
         <Background />
 
-            <div className="sidebar-header">
-                <img src="https://static.vecteezy.com/system/resources/thumbnails/022/721/714/small/youtube-logo-for-popular-online-media-content-creation-website-and-application-free-png.png" />
-                <button className='close-btn'>
-                    <FaTimes />
-                </button>
-            </div>
-
-            <ul className='links'>
-
-            </ul>
-
+        <div className="sidebar-header">
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/022/721/714/small/youtube-logo-for-popular-online-media-content-creation-website-and-application-free-png.png" />
+          <button className="close-btn">
+            <FaTimes />
+          </button>
         </div>
+
+        <ul className="links">
+            {sidebarLinks && sidebarLinks.map((link)=>{
+                const {url,text, icon} = link
+                return (
+                    <li>
+                        <a href={url}>{icon} {text}</a>
+                    </li>
+                )
+            })}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
--- 10/src/data.js
+++ 10/src/data.js
@@ -1,30 +1,39 @@
-import {FaYoutube,FaFacebook,FaTwitter,FaLinkedin,FaHome,FaUserFriends,FaFolderOpen,FaCalendarAlt,FaWpforms} from 'react-icons/fa'
-
+import React from "react";
+import {
+  FaYoutube,
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,
+} from "react-icons/fa";
 
 export const links = [
-     {
-          id: 1,
-          url: '/',
-          text: 'خانه',
-          icon: <FaHome />
-     },
-     {
-          id: 2,
-          url: '/tean',
-          text: 'تیم ما',
-          icon: <FaUserFriends />
-     },
-     {
-          id: 3,
-          url: '/projects',
-          text: 'پروژه ها',
-          icon: <FaFolderOpen />
-     },
-     {
-          id: 4,
-          url: '/docs',
-          text: 'پرونده ها',
-          icon: <FaWpforms />
-     },
-   
-]
+  {
+    id: 1,
+    url: "/",
+    text: "خانه",
+    icon: <FaHome />,
+  },
+  {
+    id: 2,
+    url: "/tean",
+    text: "تیم ما",
+    icon: <FaUserFriends />,
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "پروژه ها",
+    icon: <FaFolderOpen />,
+  },
+  {
+    id: 4,
+    url: "/docs",
+    text: "پرونده ها",
+    icon: <FaWpforms />,
+  },
+];
