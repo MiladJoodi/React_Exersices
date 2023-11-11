0cd735f3a9d9637fc61ec557bcc540228469cac1
Milad Joodi <winbeta.com@gmail.com>
2023-11-12 01:52:56 +0330

    Finish Sidebar project.

--- 10/src/Components/Home/Home.js
+++ 10/src/Components/Home/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Home.css";
 import { FaBars } from "react-icons/fa";
+import {useGlobalContext } from "../context/context";
 
 export default function Home() {
+
+  // const data = useGlobalContext()
+  const {openSidebar} = useGlobalContext()
+
+  
   return (
     <div>
-      <FaBars className="toggle-btn" />
+      <FaBars className="toggle-btn" onClick={openSidebar} />
     </div>
   );
 }
--- 10/src/Components/Sidebar/Sidebar.js
+++ 10/src/Components/Sidebar/Sidebar.js
@@ -3,20 +3,24 @@ import "./Sidebar.css";
 import { FaTimes } from "react-icons/fa";
 import Background from "./Background";
 import {links} from "../../data";
+import {useGlobalContext } from "../context/context";
 
 export default function Sidebar() {
+    
   const [sidebarLinks, setSidebarLinks] = useState(links);
 
-//   console.log(sidebarLinks);
+
+  const {isOpenSidebar, closeSidebar} = useGlobalContext()
+  
 
   return (
     <div className="sidebar">
-      <div className="container">
+      <div className={`${isOpenSidebar ? 'container show-sidebar' : 'container'}`}>
         <Background />
 
         <div className="sidebar-header">
           <img src="https://static.vecteezy.com/system/resources/thumbnails/022/721/714/small/youtube-logo-for-popular-online-media-content-creation-website-and-application-free-png.png" />
-          <button className="close-btn">
+          <button className="close-btn" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
