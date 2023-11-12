d7cd742bb36e7540706b96770df3ea3884892ecd
Milad Joodi <winbeta.com@gmail.com>
2023-11-12 18:44:53 +0330

    start new

---
+++ 11/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { Icon } from "react-icons-kit";
+import { eye } from "react-icons-kit/feather/eye";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+
+function App() {
+
+
+  return (
+    <div className="wrapper">
+      <h2>لیست کتاب ها</h2>
+      <p>کتاب جدید خود را به کتابخانه اضافه کنید</p>
+    </div>
+  );
+}
+
+export default App;
