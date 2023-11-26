041b80f2e1b93d12971f667689028dd9fd26eb53
Milad Joodi <winbeta.com@gmail.com>
2023-11-26 20:01:04 +0330

    ♥

---
+++ 16/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import data from './data'
+import "./App.css";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+
+function App() {
+
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+
+  return (
+    <div className="section">
+      <div className="title">
+        <span>نظرات مشتریان</span>
+      </div>
+      <div className="section-center">
+        {
+          people.map((person, personIndex) => {
+            const { id, image, title, qoute } = person;
+            return (
+              <article key={id}>
+                <img src={image} alt={title} className="person-img" />
+                <h4>{title}</h4>
+                <p className="title">{title}</p>
+                <p className="qoute">{qoute}</p>
+              </article>
+            )
+          })
+        }
+
+        <button className="next"><FiChevronLeft /></button>
+        <button className="prev"><FiChevronRight /></button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
