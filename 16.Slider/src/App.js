05883259530b2c2f265a72bb4df1639ae574b04a
Milad Joodi <winbeta.com@gmail.com>
2023-11-29 17:41:56 +0330

    ♥

--- 16.Slider/src/App.js
+++ 16.Slider/src/App.js
@@ -19,9 +19,10 @@ function App() {
   }, [index, people])
 
   useEffect(()=>{
-  setInterval(()=>{
+  let slider = setInterval(()=>{
     setIndex(index+1)
   },3000)
+  return()=> clearInterval(slider)
   }, [index])
 
 
