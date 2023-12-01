b8cf1e1cf2841cd795c1515793be5d57388524b3
Milad Joodi <winbeta.com@gmail.com>
2023-12-02 00:35:29 +0330

    up

--- 18-2/src/components/Products.js
+++ 18-2/src/components/Products.js
@@ -1,22 +1,59 @@
 import React from 'react'
 
 function Products() {
-  return (
-    <div>
-        <ul className='products'>
-            <li>
-                <div className='product'>
-                    <img src="images/mobile1.jpg" />
-                    <p>محصول اول</p>
-                    <div className='product-price'>
-                        <button>افزودن به سبد خرید</button>
-                        <div className='price'>1600000</div>
+    return (
+        <div>
+            <ul className='products'>
+                <li>
+                    <div className='product'>
+                        <img src="images/mobile1.jpg" />
+                        <p>محصول اول</p>
+                        <div className='product-price'>
+                            <button>افزودن به سبد خرید</button>
+                            <div className='price'>1600000</div>
+                        </div>
                     </div>
-                </div>
-            </li>
-        </ul>
-    </div>
-  )
+                </li>
+                <li>
+                    <div className='product'>
+                        <img src="images/mobile1.jpg" />
+                        <p>محصول اول</p>
+                        <div className='product-price'>
+                            <button>افزودن به سبد خرید</button>
+                            <div className='price'>1600000</div>
+                        </div>
+                    </div>
+                </li><li>
+                    <div className='product'>
+                        <img src="images/mobile1.jpg" />
+                        <p>محصول اول</p>
+                        <div className='product-price'>
+                            <button>افزودن به سبد خرید</button>
+                            <div className='price'>1600000</div>
+                        </div>
+                    </div>
+                </li><li>
+                    <div className='product'>
+                        <img src="images/mobile1.jpg" />
+                        <p>محصول اول</p>
+                        <div className='product-price'>
+                            <button>افزودن به سبد خرید</button>
+                            <div className='price'>1600000</div>
+                        </div>
+                    </div>
+                </li><li>
+                    <div className='product'>
+                        <img src="images/mobile1.jpg" />
+                        <p>محصول اول</p>
+                        <div className='product-price'>
+                            <button>افزودن به سبد خرید</button>
+                            <div className='price'>1600000</div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Products
