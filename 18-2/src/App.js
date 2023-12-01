ebb5d310aceb0afe30d720e357a414e9ed91e6eb
Milad Joodi <winbeta.com@gmail.com>
2023-12-01 17:39:53 +0330

    wow

---
+++ 18-2/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+import Filter from "./components/Filter";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+function App() {
+
+
+
+  return (
+    <div className="container">
+      <header>
+        <a href="">فروشگاه من</a>
+      </header>
+      <main>
+        <div className="content">
+        <div className="main">
+
+          <Filter />
+          <Products />
+
+        </div>
+        <div className="sidebar">
+          <Cart />
+        </div>
+        </div>
+      </main>
+      <footer>
+        طراحی و توسعه توسط من
+      </footer>
+    </div>
+  );
+}
+
+export default App;
