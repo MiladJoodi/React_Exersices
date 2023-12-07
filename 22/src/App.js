b6f1425422ebf369f512682e743a1fcf5bb67e3d
Milad Joodi <winbeta.com@gmail.com>
2023-12-07 19:52:27 +0330

    109 finished

--- 22/src/App.js
+++ 22/src/App.js
@@ -4,17 +4,26 @@ import { Users } from "./users";
 import Table from "./Table";
 
 function App() {
+  const [query, setQuery] = useState("");
 
-  const [query, setQuery] = useState("")
-
-  const search = (data)=> {
-    return data.filter((item)=> item.first_name.toLowerCase().includes(query))
-  }
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+        item.first_name.toLowerCase().includes(query) ||
+        item.last_name.toLowerCase().includes(query) ||
+        item.email.toLowerCase().includes(query)
+    );
+  };
 
   return (
     <div className="app">
-      <input type="text" placeholder="جستجو..." className="search" onChange={(e)=> setQuery(e.target.value)}  />
-    <Table data={search(Users)} />
+      <input
+        type="text"
+        placeholder="جستجو..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
     </div>
   );
 }
