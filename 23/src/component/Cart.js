b4c53f627779f59b82102f7181e9b1e21a5f903e
Milad Joodi <winbeta.com@gmail.com>
2023-12-08 12:25:34 +0330

    Redux

--- 23/src/component/Cart.js
+++ 23/src/component/Cart.js
@@ -1,11 +1,42 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Data } from "../data";
+import formatCurrency from "../util.js";
+import { useSelector, useDispatch } from "react-redux";
+
 
 export default function Cart() {
+
+  const cart = useSelector((store)=> store);
+  console.log(cart)
+  const dispatch = useDispatch();
+
+
   return (
     <>
       <Navbar />
-
+      <div className="cart">
+        {Data.map((item) => {
+          return (
+            <div className="cart-item" key={item.id}>
+              <img src={item.image} />
+              <div className="cart-item-text">
+                <div className="cart-item-info">
+                  <h4>{item.title}</h4>
+                  <h5>{formatCurrency(item.price)}</h5>
+                </div>
+                <div className="add-to-cart">
+                  <button
+                    onClick={() => dispatch({ type: "ADD", payload: item })}
+                  >
+                    افزودن سبد خرید
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
