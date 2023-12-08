20b5ff86d06d1f90a816dc781aed03dd31e32374
Milad Joodi <winbeta.com@gmail.com>
2023-12-08 19:56:50 +0330

    120 Finish

--- 24/src/App.js
+++ 24/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
+import Products from "./component/Products";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <div className="container">
         <BrowserRouter>
           <Header />
+          <Routes>
+            <Route path="/products" element={<Products />}></Route>
+          </Routes>
         </BrowserRouter>
       </div>
     </div>
---
+++ 24/src/component/Context.js
@@ -0,0 +1,6 @@
+import React, { createContext, useState } from "react";
+
+export const DataContext = createContext()
+export const DataProvider = ()=>{
+    const [product, setProducts] = useState()
+}
