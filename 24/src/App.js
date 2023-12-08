83654e9248e143fd837849e7358608dafd532367
Milad Joodi <winbeta.com@gmail.com>
2023-12-08 21:46:00 +0330

    120

--- 24/src/App.js
+++ 24/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
 import Products from "./component/Products";
+import { DataProvider } from "./component/Context";
 
 function App() {
   return (
-    <div className="main-website">
+   <DataProvider>
+     <div className="main-website">
       <div className="container">
         <BrowserRouter>
           <Header />
@@ -16,6 +18,7 @@ function App() {
         </BrowserRouter>
       </div>
     </div>
+   </DataProvider>
   );
 }
 
