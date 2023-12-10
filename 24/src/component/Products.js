41be65d4dd2e179d44e1658b74b219d826e09039
Milad Joodi <winbeta.com@gmail.com>
2023-12-10 17:17:31 +0330

    ♥

--- 24/src/component/Products.js
+++ 24/src/component/Products.js
@@ -6,25 +6,29 @@ import formatCurrency from '../util'
 function Products() {
   const value = useContext(DataContext);
   const [products, setProducts] = value.products;
-  console.log(products);
-
+  
+  // console.log(products);
+  
   return (
     <div className="products">
-      {products.map((product) => (
-        <div className="card" key={product._id}>
-          <Link to="/">
-            <img src={product.images[0]} />
-          </Link>
-          <div className="box">
-            <h3>
-              <Link>{product.title}</Link>
-            </h3>
-            <p>{product.description}</p>
-            <h4>{formatCurrency(product.price)}</h4>
-            <button>افزودن به سبد خرید</button>
+
+      {
+        products.map(product=>(
+          <div className="card" key={product._id}>
+            <Link to="/">
+            <img src={product.images[0]}  />
+            </Link>
+            <div className="box">
+              <h3>
+                <Link to="/">{product.title}</Link>
+              </h3>
+              <p>{product.description}</p>
+              <h4>{formatCurrency(product.price)}</h4>
+              <button>افزودن به سبد خرید</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      }
     </div>
   );
 }
